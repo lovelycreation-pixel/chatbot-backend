13add960d1551dafdc1fbb12739f16996ca7b8f4
lovelycreation-pixel <zaberhkhan@gmail.com>
2026-01-11 21:56:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
