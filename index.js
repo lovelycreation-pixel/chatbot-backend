eb8806d5cd504ac65b6bc77478d0d912ba4faf22
lovelycreation-pixel <zaberhkhan@gmail.com>
2026-01-12 11:46:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.post("/chat", async (req, res) => {
+  const { message, clientId } = req.body;
 
+  if (!message) {
+    return res.json({ reply: "No message received" });
+  }
+
+  // TEMP response (we will improve later)
+  res.json({
+    reply: `You said: ${message}`
+  });
+});
 app.get("/", (req, res) => {
   res.send("Chatbot backend running");
 });
