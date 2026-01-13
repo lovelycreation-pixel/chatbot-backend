265c2e6e5128468becbd4df6fff891582a859a1b
lovelycreation-pixel <zaberhkhan@gmail.com>
2026-01-13 20:30:40 +0600

    Update basicMatcher.js

--- sevices/basicMatcher.js
+++ sevices/basicMatcher.js
@@ -1,1 +1,27 @@
+const { normalizeText, splitSentences } = require("../utils/textUtils");
 
+function getBestMatch(userMessage, adminInfo, fallback) {
+  const userWords = normalizeText(userMessage);
+  const sentences = splitSentences(adminInfo);
+
+  let bestSentence = "";
+  let highestScore = 0;
+
+  for (const sentence of sentences) {
+    let score = 0;
+    const sentenceLower = sentence.toLowerCase();
+
+    userWords.forEach(word => {
+      if (sentenceLower.includes(word)) score++;
+    });
+
+    if (score > highestScore) {
+      highestScore = score;
+      bestSentence = sentence;
+    }
+  }
+
+  return highestScore > 0 ? bestSentence : fallback;
+}
+
+module.exports = getBestMatch;
