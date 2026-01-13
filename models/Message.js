b60876ee2ea967a9137282e04c416e16f3a96246
lovelycreation-pixel <zaberhkhan@gmail.com>
2026-01-13 21:13:28 +0600

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,32 @@
+const mongoose = require("mongoose");
 
+const MessageSchema = new mongoose.Schema({
+  clientId: {
+    type: String,
+    required: true,
+    index: true
+  },
+
+  role: {
+    type: String,
+    enum: ["user", "bot"],
+    required: true
+  },
+
+  content: {
+    type: String,
+    required: true
+  },
+
+  size: {
+    type: Number, // bytes
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
